8d8a1dc0d96c49e6f4305987ffc86b1dc5f2b639
jibunoronyinyepraise-a11y <you@example.com>
2025-11-18 12:24:59 +0100

    QuestionPageSetup

---
+++ src/pages/Questionpage.jsx
@@ -0,0 +1,14 @@
+function QuestionPage() {
+  return (
+    <>
+      <div
+        classname="
+          w-[100%]
+          h-[100vh]
+          bg-[red]
+          "
+      ></div>
+    </>
+  );
+}
+export default QuestionPage;
